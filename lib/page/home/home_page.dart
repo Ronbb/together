f747204d3edbe9627bf507dc6fa3b4d1680863d1
unknown <wangruobiao@supcon.com>
2020-07-20 10:45:39 +0800

    Map develop

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,6 +1,8 @@
-
 import 'package:flutter/material.dart';
+import 'package:latlng/latlng.dart';
+import 'package:map/map.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:together/ignore/license.dart';
 
 class HomePage extends StatefulWidget {
   // ignore: prefer_const_constructors_in_immutables
@@ -11,73 +13,92 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Meeting> meetings;
+  // List<Meeting> meetings;
+  final controller = MapController(
+    location: LatLng(116.481485, 39.990464),
+  );
 
   @override
   Widget build(BuildContext context) {
+    controller.location = LatLng(116.481485, 39.990464);
     return Scaffold(
-      body: SfCalendar(
-        view: CalendarView.schedule,
-        dataSource: MeetingDataSource(_getDataSource()),
-        // by default the month appointment display mode set as Indicator, we can
-        // change the display mode as appointment using the appointment display mode
-        // property
-        monthViewSettings: MonthViewSettings(
-          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
-        ),
+      body: Map(
+        controller: controller,
+        provider: const AMapProvider(),
       ),
+      // body: SfCalendar(
+      //   view: CalendarView.schedule,
+      //   dataSource: MeetingDataSource(_getDataSource()),
+      //   // by default the month appointment display mode set as Indicator, we can
+      //   // change the display mode as appointment using the appointment display mode
+      //   // property
+      //   monthViewSettings: MonthViewSettings(
+      //     appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+      //   ),
+      // ),
     );
   }
 
-  List<Meeting> _getDataSource() {
-    meetings = <Meeting>[];
-    final DateTime today = DateTime.now();
-    final DateTime startTime =
-        DateTime(today.year, today.month, today.day, 9, 0, 0);
-    final DateTime endTime = startTime.add(const Duration(hours: 2));
-    meetings.add(Meeting(
-        'Conference', startTime, endTime, const Color(0xFF0F8644), false));
-    return meetings;
-  }
+//   List<Meeting> _getDataSource() {
+//     meetings = <Meeting>[];
+//     final DateTime today = DateTime.now();
+//     final DateTime startTime =
+//         DateTime(today.year, today.month, today.day, 9, 0, 0);
+//     final DateTime endTime = startTime.add(const Duration(hours: 2));
+//     meetings.add(Meeting(
+//         'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+//     return meetings;
+//   }
+// }
+
+// class MeetingDataSource extends CalendarDataSource {
+//   MeetingDataSource(List<Meeting> source) {
+//     appointments = source;
+//   }
+
+//   @override
+//   DateTime getStartTime(int index) {
+//     return appointments[index].from;
+//   }
+
+//   @override
+//   DateTime getEndTime(int index) {
+//     return appointments[index].to;
+//   }
+
+//   @override
+//   String getSubject(int index) {
+//     return appointments[index].eventName;
+//   }
+
+//   @override
+//   Color getColor(int index) {
+//     return appointments[index].background;
+//   }
+
+//   @override
+//   bool isAllDay(int index) {
+//     return appointments[index].isAllDay;
+//   }
+// }
+
+// class Meeting {
+//   Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+//   String eventName;
+//   DateTime from;
+//   DateTime to;
+//   Color background;
+//   bool isAllDay;
 }
 
-class MeetingDataSource extends CalendarDataSource {
-  MeetingDataSource(List<Meeting> source) {
-    appointments = source;
-  }
+class AMapProvider extends MapProvider {
+  const AMapProvider();
 
   @override
-  DateTime getStartTime(int index) {
-    return appointments[index].from;
+  ImageProvider getTile(int x, int y, int z) {
+    //Can also use CachedNetworkImageProvider.
+    return NetworkImage(
+        'https://restapi.amap.com/v3/staticmap?location=$x,$y&zoom=10&size=2000*2000&markers=mid,,A:$x,$y&key=$AMAP_KEY');
   }
-
-  @override
-  DateTime getEndTime(int index) {
-    return appointments[index].to;
-  }
-
-  @override
-  String getSubject(int index) {
-    return appointments[index].eventName;
-  }
-
-  @override
-  Color getColor(int index) {
-    return appointments[index].background;
-  }
-
-  @override
-  bool isAllDay(int index) {
-    return appointments[index].isAllDay;
-  }
-}
-
-class Meeting {
-  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
-
-  String eventName;
-  DateTime from;
-  DateTime to;
-  Color background;
-  bool isAllDay;
 }
