1bc3a984c88686c770294f13bf245d40678b550f
ronbb <ronbiaobiao@gmail.com>
2020-08-02 23:35:38 +0800

    Todo Dialog UI (Focus Part)

--- lib/layout/home/add_todo_dialog.dart
+++ lib/layout/home/add_todo_dialog.dart
@@ -12,23 +12,36 @@ class TodoInfo {}
 
 class _AddTodoDialogState extends State<AddTodoDialog> {
   TextEditingController _textEditingController;
-  bool hasContent;
+  FocusNode _focusNode;
+  bool _hasContent;
+  bool _showMore;
 
   Color get suffixIconDisabledColor => Theme.of(context).disabledColor;
   Color get suffixIconAccentColor => Theme.of(context).accentColor;
   Color get suffixIconColor =>
-      hasContent ?? false ? suffixIconAccentColor : suffixIconDisabledColor;
+      _hasContent ?? false ? suffixIconAccentColor : suffixIconDisabledColor;
 
   Color get prefixIconColor => Theme.of(context).iconTheme.color;
 
+  double get marginBottom =>
+      _showMore ?? false ? 0 : MediaQuery.of(context).viewInsets.bottom;
+
   @override
   void initState() {
-    hasContent = false;
+    _hasContent = false;
+    _showMore = false;
+
+    _focusNode = FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        _showMore = !_focusNode.hasFocus;
+      });
+    });
 
     _textEditingController = TextEditingController();
     _textEditingController.addListener(() {
       setState(() {
-        hasContent = _textEditingController.text?.isNotEmpty ?? false;
+        _hasContent = _textEditingController.text?.isNotEmpty ?? false;
       });
     });
 
@@ -45,67 +58,86 @@ class _AddTodoDialogState extends State<AddTodoDialog> {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.bottomCenter,
-      margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      margin: EdgeInsets.only(bottom: marginBottom),
       decoration: BoxDecoration(color: Colors.grey),
-      child: Material(
-        elevation: 16,
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextField(
-                autofocus: true,
-                controller: _textEditingController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(
-                    top: 16,
-                    bottom: 12,
-                    left: 12,
-                    right: 12,
-                  ),
-                  focusedBorder: InputBorder.none,
-                  prefixIcon: Icon(
-                    Icons.check_circle_outline,
-                    color: prefixIconColor,
-                  ),
-                  hintText: "Helper",
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      Icons.arrow_forward,
-                      color: suffixIconColor,
+      child: GestureDetector(
+        onTap: () => _focusNode.unfocus(),
+        child: Material(
+          elevation: 16,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                  autofocus: true,
+                  controller: _textEditingController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(
+                      top: 16,
+                      bottom: 12,
+                      left: 12,
+                      right: 12,
                     ),
-                    onPressed: () {},
+                    focusedBorder: InputBorder.none,
+                    prefixIcon: Icon(
+                      Icons.add_circle_outline,
+                      color: prefixIconColor,
+                    ),
+                    hintText: "Helper",
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        Icons.arrow_forward,
+                        color: suffixIconColor,
+                      ),
+                      onPressed: () {},
+                    ),
                   ),
+                  focusNode: _focusNode,
+                  textAlignVertical: TextAlignVertical.center,
+                  textCapitalization: TextCapitalization.sentences,
+                  textInputAction: TextInputAction.go,
                 ),
-                textAlignVertical: TextAlignVertical.center,
-                textCapitalization: TextCapitalization.sentences,
-                textInputAction: TextInputAction.go,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.calendar_today),
-                    label: Text('Date'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.calendar_today),
+                      label: Text('Date'),
+                    ),
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.repeat),
+                      label: Text('Repeat'),
+                    ),
+                    FlatButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.alarm_add),
+                      label: Text('Alarm'),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.more_vert),
+                      onPressed: _showMoreOptions,
+                    )
+                  ],
+                ),
+                if (_showMore)
+                  Container(
+                    height: 192,
                   ),
-                  FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.repeat),
-                    label: Text('Repeat'),
-                  ),
-                  FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.alarm_add),
-                    label: Text('Alarm'),
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _showMoreOptions() {
+    setState(() {
+      _focusNode.unfocus();
+      _showMore = true;
+    });
   }
 }
